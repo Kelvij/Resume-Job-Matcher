0f3fb475eef24264fd25611c771dbea06048474f
Kelvij <jameskelvin.tiku@gmail.com>
2026-09-19 11:07:34 +0530

    Set basename for BrowserRouter in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,7 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Resume-Job-Matcher">
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<HomePage />} />
